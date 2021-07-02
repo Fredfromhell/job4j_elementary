0d4d91450a120f198d9c7a4dd118230a683aa1fd
FredfromHell <bespalov@avtocenter.ru>
2021-07-02 11:17:28 +0300

    8. Стороны прямоугольника. [#137735]

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double h1 = k+1;
+        double h2 = h1*2;
+        double h3 = p/h2;
+        double L = h3*k;
+        double result1 = L*h3;
+        return result1;
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+}
