55b7e0e5123b99dffd2f734027202f86bc13251f
FredfromHell <bespalov@avtocenter.ru>
2021-07-12 15:06:00 +0300

    1. Модульные тесты. [#159435]

--- test/ru/job4j/ConverterTest.java
+++ test/ru/job4j/ConverterTest.java
@@ -10,7 +10,7 @@ public class ConverterTest {
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
-        float eps = 0.0001f;
+        float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
     }
 
@@ -19,7 +19,7 @@ public class ConverterTest {
         float in = 180;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
-        float eps = 0.0001f;
+        float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
     }
 }
