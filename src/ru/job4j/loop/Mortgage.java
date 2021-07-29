b1502463456ea7edd850dbd78fd5a8da5b6cf8bd
FredfromHell <bespalov@avtocenter.ru>
2021-07-29 16:46:55 +0300

    5.7. Ипотека [#156315]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        double total = amount + (amount * (percent / 100));
+        while (total > 0) {
+            total = (total - salary);
+            total = total * (percent / 100) + total;
+            year = year + 1;
+        }
+        return year;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(year(100, 70, 50));
+    }
+}
