91c0da88f1c401c20d526a870b488ce64126483c
FredfromHell <bespalov@avtocenter.ru>
2021-07-29 16:05:04 +0300

    5.6. Простые числа [#156314]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -16,7 +16,7 @@ public class CheckPrimeNumber {
     }
 
     public static void main(String args[]) {
-        System.out.println(check(1));
+        System.out.println(check(11));
     }
 }
 
--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -13,6 +13,6 @@ public class PrimeNumber {
         return count;
     }
     public static void main (String args[]) {
-        System.out.println(calc(5));
+        System.out.println(calc(11));
     }
 }
