991bc0bba4c56bc3d0826c4dfecd7a285e395d58
DieHard <-_->
2025-03-17 13:59:39 +0300

    3. Перегрузить метод max для трех чисел [#122644 #205669]

---
+++ src/main/java/ru/job4j/Task15.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+
+public class Task15 {
+    public static void main(String[] args) {
+        String source = "+!?";
+        char[] ch = source.toCharArray();
+        System.out.print(ch[0]);
+        System.out.print(ch[0]);
+        System.out.print(ch[0]);
+        System.out.print(ch[1]);
+        System.out.print(ch[1]);
+        System.out.print(ch[1]);
+        System.out.print(ch[1]);
+        System.out.print(ch[2]);
+        System.out.print(ch[2]);
+
+
+
+    }
+}
--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int one, int two) {
-        return one > two ? one : two;
+        return Math.max(one, two);
     }
 
     public static int max(int one, int two, int three) {
